905cbc14797f0afa2211a1a1a8fafef6d0f207d2
krishnakandula10 <37396580+krishnakandula10@users.noreply.github.com>
2018-03-16 17:07:18 +0530

    Add files via upload

---
+++ project/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="nav">
+            <nav>
+            <h1><b>Dream Homes<b></h1>
+                <ul>
+                   <a href="home.html">Home</a>
+              	   <a href="login.html">LogIn</a>
+                    <a href="signup.html">SignUp</a>
+                </ul>
+            </nav>
+            </div>
+<form>
+  <div class="hlogin">
+      <h3>Login</h3>
+      <tr>
+      <td>
+      Email:
+      <td>
+      <td>
+      <input type="text" placeholder="Email" name="">
+      </td>
+      </tr>
+      <br>
+      <tr>
+      <td>
+      Password:
+      <td>
+      <td>
+      <input type="password" placeholder="*******" password="">
+      </td>
+      </tr>
+      <br>
+       <input type="submit" value="login" class="login" name="login">
+</form>    
+      </div>
+
+    </body>
+</html>
---
+++ project/signup.php
@@ -0,0 +1,9 @@
+<?php
+	include "connect.php";
+	if(isset($_POST['sub'])){
+		$a=$_POST['username'];
+		$b=$_POST['email'];
+		$c=$_POST['password']
+		
+	}
+?>
